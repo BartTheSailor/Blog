57aeba90993618630017e9b4efcb3eb9ca15d48a
Bartek <bkurzawski@onet.pl>
2019-02-16 16:22:38 +0100

    Use ActiveJob for mailers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,11 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {address: 'localhost', port: 1025}
+
+  
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -59,5 +64,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { host: 'stormy-woodland-15808.herokuapp.com', port: 80 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
